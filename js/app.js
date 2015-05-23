a1a16120da2f59a9e26ae14fb0e8f560d0cceed9
James Byrum <me@jimmybyrum.com>
2015-05-23 16:28:01 -0700

    first draft of slider

--- js/app.js
+++ js/app.js
@@ -1,5 +1,19 @@
 $(document).ready(function() {
   $(document).on('click', '.view-slides', function() {
     $('html').addClass('slideshowing');
+  }).on('keypress', function(e) {
+    if (e.keyCode === 27) {
+      $('html').removeClass('slideshowing');
+    }
+  }).on('click', '.modal', function(e) {
+    var $target = $(e.target);
+    if ($target.hasClass('modal')) {
+      $('html').removeClass('slideshowing');
+    }
+  });
+  $('.slideshow').slick({
+    dots: true,
+    prevArrow: '<i class="slick-prev clickable fa fa-arrow-left"></i>',
+    nextArrow: '<i class="slick-next clickable fa fa-arrow-right"></i>'
   });
 });
