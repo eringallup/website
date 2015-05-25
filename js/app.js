6f5e6a49c6be545de6f5faf47176eb3d29839ee6
James Byrum <me@jimmybyrum.com>
2015-05-25 13:16:47 -0700

    more responsive stuff

--- js/app.js
+++ js/app.js
@@ -16,15 +16,17 @@ $(document).ready(function() {
       $('html').removeClass('slideshowing');
     }
   });
-  $('.slideshow').slick({
-    mobileFirst: true,
-    fade: true,
-    prevArrow: '<i class="left-arrow clickable"></i>',
-    nextArrow: '<i class="right-arrow clickable"></i>'
-  });
-  $(document).on('keydown.slick', '.slick-list', function(e) {
-    if (e.keyCode === 27) {
-      $('html').removeClass('slideshowing');
-    }
-  });
+  if ($.fn.slick) {
+    $('.slideshow').slick({
+      mobileFirst: true,
+      fade: true,
+      prevArrow: '<i class="left-arrow clickable"></i>',
+      nextArrow: '<i class="right-arrow clickable"></i>'
+    });
+    $(document).on('keydown.slick', '.slick-list', function(e) {
+      if (e.keyCode === 27) {
+        $('html').removeClass('slideshowing');
+      }
+    });
+  }
 });
